385b5390e0ca256f3192800712a8c56237b1fa82
maicol avila <maavilav.2001@gmail.com>
2024-02-11 18:01:55 -0500

    Update CSS styles and navigation component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${lato.className} bg-background-100`}>
         <div className="flex flex-row bg-background-100 px-8 justify-center items-center">
-          <div className="flex flex-row max-w-screen-lg max-h-screen">
+          <div className="flex flex-row max-w-screen-xl max-h-screen">
             <div className="flex flex-col">
               <Image
                 className="rounded-full"
--- src/components/navigation/Navigation.component.tsx
+++ src/components/navigation/Navigation.component.tsx
@@ -23,11 +23,18 @@ export default function Navigation() {
         if (entry.isIntersecting) {
           const id = entry.target.getAttribute("id");
           const link = document.querySelector(`[href="#${id}"]`);
+          console.log(link);
+          if (link) link.classList.remove("text-xl");
+          if (link) link.classList.remove("text-blue-100");
           if (link) link.classList.add("text-2xl");
+          if (link) link.classList.add("background-text-animation");
         } else {
           const id = entry.target.getAttribute("id");
           const link = document.querySelector(`[href="#${id}"]`);
+          if (link) link.classList.add("text-xl");
+          if (link) link.classList.add("text-blue-100");
           if (link) link.classList.remove("text-2xl");
+          if (link) link.classList.remove("background-text-animation");
         }
       });
     }, options);
@@ -38,14 +45,14 @@ export default function Navigation() {
   };
 
   return (
-    <div className="border-solid border-2 border-l-blue-100 px-4">
+    <div className="border-solid border-2 border-l-blue-100 border-b-0 border-r-0 border-t-0 px-4">
       <ul className="gap-4 flex flex-col">
         {routes.map((route) => {
           return (
             <li key={route.path}>
               <Link
                 href={route.path}
-                className="text-xl text-blue-100 transition duration-300 ease-in-out"
+                className="text-xl text-blue-100"
               >
                 - {route.name}
               </Link>
